5773e46967560873c91eee78a55f9280dd417b12
dvpappas <dvpappas89@gmail.com>
2023-05-29 12:28:27 +0300

    optimized models for speed

--- batch_classifier.py
+++ batch_classifier.py
@@ -1,10 +1,11 @@
 
-import argparse, pickle
+import argparse, pickle, time
 from pprint import pprint
-from resources.code.box_1 import K1_model
-from resources.code.box_2 import KT_matcher
-from resources.code.box_3 import MyGuidedLDA
-from resources.code.box_4 import K4_model
+from the_vectorizer import Vectorizer
+from box_1 import K1_model
+from box_2 import KT_matcher
+from box_3 import MyGuidedLDA
+from box_4 import K4_model
 from tqdm import tqdm
 from collections import Counter
 
@@ -15,7 +16,6 @@ logging.basicConfig(filename='sdg_api.log', level=logging.INFO, format="%(asctim
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path",          type=str,   default="./data.txt",   help="doi|~|text",              required=False)
-parser.add_argument("--delimeter",          type=str,   default="|~|",          help="doi|~|text",              required=False)
 parser.add_argument("--out_path",           type=str,   default="./sdg_out.p",  help="{doi:Counter()}",         required=False)
 parser.add_argument("--log_path",           type=str,   default='sdg_batch_models.log',  help="The path for the log file.", required=False)
 parser.add_argument("--guided_thres",       type=float, default=0.4,            help="",                        required=False)
@@ -23,7 +23,7 @@ parser.add_argument("--batch_size",         type=int,   default=100,            
 parser.add_argument("--BERT_thres",         type=float, default=0.7,            help="",                        required=False)
 parser.add_argument("--BERT_thres_old",     type=float, default=0.95,           help="",                        required=False)
 parser.add_argument("--BERT_ATT_thres_old", type=float, default=0.98,           help="",                        required=False)
-parser.add_argument("--ensemble_agreement", type=int,   default=3,              help="",                        required=False)
+parser.add_argument("--KT_parallel_jobs",   type=int,   default=10,             help="",                        required=False)
 args                = parser.parse_args()
 logging.basicConfig(filename=args.log_path, level=logging.INFO, format="%(asctime)s;%(levelname)s;%(message)s")
 
@@ -36,56 +36,69 @@ BERT_ATT_thres_old  = args.BERT_ATT_thres_old   # 0.98
 data_path           = args.data_path
 batch_size          = args.batch_size
 out_path            = args.out_path
+parallel_jobs       = args.KT_parallel_jobs
 delimeter           = args.delimeter
 ensemble_agreement  = args.ensemble_agreement
 
 ################################################################################################################
 
+distilbert_vectorizer   = Vectorizer(model_name="distilbert-base-uncased")
+bert_vectorizer         = Vectorizer(model_name='bert-base-uncased')
+
+################################################################################################################
+
 print('loading box 1')
-k1_1 = K1_model(
-    model_name="distilbert-base-uncased",
-    hidden=100,
-    resume_from='./resources/models/distilbert-base-uncased_100_5e-05_29_84_85.pth.tar'
-)
-k1_2 = K1_model(
-    model_name="distilbert-base-uncased",
-    hidden=50,
-    resume_from='./resources/models/distilbert-base-uncased_50_5e-05_23_83_84.pth.tar'
-)
-k1_3 = K1_model(
-    model_name="bert-base-uncased",
-    hidden=100,
-    resume_from='./resources/models/bert-base-uncased_100_5e-05_16_84_84.pth.tar'
-)
+k1_1 = K1_model(hidden=100, resume_from='./models/distilbert-base-uncased_100_5e-05_29_84_85.pth.tar')
+k1_2 = K1_model(hidden=50,  resume_from='./models/distilbert-base-uncased_50_5e-05_23_83_84.pth.tar')
+k1_3 = K1_model(hidden=100, resume_from='./models/bert-base-uncased_100_5e-05_16_84_84.pth.tar')
 
 print('loading box 2')
-kt_match    = KT_matcher(kt_fpath = './resources/data/sdg_vocabulary.xlsx')
+kt_match    = KT_matcher(kt_fpath = './models/sdg_vocabulary.xlsx', parallel_jobs = parallel_jobs)
 
 print('loading box 3')
 glda        = MyGuidedLDA(
-    kt_fpath            = './resources/data/sdg_vocabulary.xlsx',
-    guided_tm_path      = './resources/models/guidedlda_model.pickle',
-    guided_tm_cv_path   = './resources/models/guidedlda_countVectorizer.pickle'
+    kt_fpath            = './models/sdg_vocabulary.xlsx',
+    guided_tm_path      = './models/guidedlda_model.pickle',
+    guided_tm_cv_path   = './models/guidedlda_countVectorizer.pickle'
 )
 
 print('loading box 4')
 k4 = K4_model(
-    model_name          = "distilbert-base-uncased",
-    resume_from_1       = './resources/models/distilbert-base-uncased_3_87_88.pth.tar',
-    resume_from_2       = './resources/models/distilbert-base-uncased_4_78_80.pth.tar'
+    resume_from_1       ='./models/distilbert-base-uncased_3_87_88.pth.tar',
+    resume_from_2       ='./models/distilbert-base-uncased_4_78_80.pth.tar'
 )
 
 ################################################################################################################
 
 def do_for_one_batch(batch_dois, batch_texts, all_file_results):
-    ################################################################################
-    kt_sdg_res      = kt_match.emit_for_abstracts(batch_texts)
-    r1              = k1_1.emit_for_abstracts(batch_texts)
-    r2              = k1_2.emit_for_abstracts(batch_texts)
-    r3              = k1_3.emit_for_abstracts(batch_texts)
-    bert_results, bert_results_att = k4.emit_for_abstracts(batch_texts)
-    guided_sdg_res  = glda.emit_for_abstracts(batch_texts)
-    ################################################################################
+    ###################################################################################
+    (distil_pooled_vecs, distil_token_vecs, distil_masks) = distilbert_vectorizer.emit_for_abstracts(batch_texts)
+    (bert_pooled_vecs, _, _) = bert_vectorizer.emit_for_abstracts(batch_texts)
+    ###################################################################################
+    start_time = time.perf_counter()
+    kt_sdg_res = kt_match.emit_for_abstracts(batch_texts)
+    print("Elapsed time 1: ", time.perf_counter() - start_time)
+    #################################
+    start_time = time.perf_counter()
+    r1 = k1_1.emit_for_abstracts(batch_texts, distil_pooled_vecs)
+    print("Elapsed time 2: ", time.perf_counter() - start_time)
+    #################################
+    start_time = time.perf_counter()
+    r2 = k1_2.emit_for_abstracts(batch_texts, distil_pooled_vecs)
+    print("Elapsed time 3: ", time.perf_counter() - start_time)
+    #################################
+    start_time = time.perf_counter()
+    r3 = k1_3.emit_for_abstracts(batch_texts, bert_pooled_vecs)
+    print("Elapsed time 4: ", time.perf_counter() - start_time)
+    #################################
+    start_time = time.perf_counter()
+    bert_results, bert_results_att = k4.emit_for_abstracts(batch_texts,distil_pooled_vecs,distil_token_vecs,distil_masks)
+    print("Elapsed time 5: ", time.perf_counter() - start_time)
+    #################################
+    start_time = time.perf_counter()
+    guided_sdg_res = glda.emit_for_abstracts(batch_texts)
+    print("Elapsed time 6: ", time.perf_counter() - start_time)
+    ###################################################################################
     for i in range(len(batch_dois)):
         bdoi                = batch_dois[i]
         bkt_sdg_res         = kt_sdg_res[i][1]
@@ -111,6 +124,8 @@ def do_for_one_batch(batch_dois, batch_texts, all_file_results):
             all_file_results[bdoi] = Counter(final_sdg_categories)
     ################################################################################
     return all_file_results
+
+################################################################################################################
 
 if __name__ == '__main__':
     batch_dois  = []
